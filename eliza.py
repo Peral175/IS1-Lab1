bd711cb1d9adba7f3d863399c9d9b9fab133dd16
Peral175 <perrardalex@yahoo.com>
2022-03-09 15:00:22 +0100

    eliza v0.1

--- eliza.py
+++ eliza.py
@@ -1,6 +1,50 @@
-print("Hello World!")
+import re
+import random
 from datetime import datetime
 
+class Eliza:
+    def __init__(self):
+        self.initials = []
+        self.finals = []
+        self.quits = []
+    
+    def load(self, filePath):
+        with open(filePath) as file:
+            for line in file:
+                if not line.strip():
+                    continue
+                tag, content = [part.strip() for part in line.split(':')]
+                if tag == 'initial':
+                    self.initials.append(content)
+                elif tag == 'final':
+                    self.finals.append(content)
+                elif tag == 'quit':
+                    self.quits.append(content)
+
+    def randInitial(self):
+        return random.choice(self.initials)
+    
+    def randFinal(self):
+        return random.choice(self.finals)
+    
+    def respond(self, text):
+        output = None
+        if text.lower() in self.quits: # to quit 
+            return output
+        output = text
+        return output
+
+    def run(self):
+        print(self.randInitial())
+        while True:
+            sent = input('> ')
+            output = self.respond(sent)
+            if output is None:
+                break
+            print(output)
+        print(self.randFinal())
+
+        # chat log
 
 def getTime():
     now = datetime.now()
@@ -12,3 +56,11 @@ psychobable = [
     ["The current time is: {0} in {1}",
     ]]
 ]
+
+def main():
+    eliza = Eliza()
+    eliza.load('textFile.txt')
+    eliza.run()
+
+if __name__ == '__main__':
+    main()
