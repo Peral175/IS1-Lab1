7d64f234e930d8a8772408798ba950f0f670e455
Peral175 <perrardalex@yahoo.com>
2022-03-10 11:17:44 +0100

    added comments and rename chat log file

--- eliza.py
+++ eliza.py
@@ -2,15 +2,17 @@ import re
 import random
 from datetime import datetime
 
-CHATLOG = "chatLog"
+CHATLOG = "chatLog.txt"             # file name for chat log
 
 class Eliza:
     def __init__(self):
-        self.initials = []
-        self.finals = []
-        self.quits = []
+        self.initials = []      # list of greetings of the chatbot
+        self.finals = []        # list of final words of the chatbot
+        self.quits = []         # list of words to quit talking to the chatbot
     
     def load(self, filePath):
+        # method load
+        # parameters: self, filePath -> file containing initials, finals and quits
         with open(filePath) as file:
             for line in file:
                 if not line.strip():
@@ -24,12 +26,15 @@ class Eliza:
                     self.quits.append(content)
 
     def randInitial(self):
+        # return random initial
         return random.choice(self.initials)
     
     def randFinal(self):
+        # return random final
         return random.choice(self.finals)
     
     def respond(self, text):
+        # ...
         output = None
         if text.lower() in self.quits: # to quit 
             return output
@@ -37,14 +42,19 @@ class Eliza:
         return output
 
     def logChat(self, filePath, line, actor):
+        # method logChat
+        # attributes:   self, 
+        #               filePath    -> where to save the conversation, 
+        #               line        -> line to be saved, 
+        #               actor       -> which actor said line
         with open(filePath, 'a') as file:
             file.write('\n'+actor+'\t'+line)
             file.close()
 
     def run(self):
-        initial = self.randInitial()
-        file = open(CHATLOG, 'w')
-        file.write('Bot: \t' + initial) # Erase content from previous chat?
+        initial = self.randInitial()        
+        file = open(CHATLOG, 'w')           
+        file.write('Bot: \t' + initial)     # Erase content from previous chat?
         file.close()       
         print(initial)
         while True:
@@ -71,9 +81,9 @@ psychobable = [
 ]
 
 def main():
-    eliza = Eliza()
-    eliza.load('textFile.txt')
-    eliza.run()
+    eliza = Eliza()             # initialize the class with empty values
+    eliza.load('textFile.txt')  # load values
+    eliza.run()                 # run Eliza chatbot
 
 if __name__ == '__main__':
     main()
